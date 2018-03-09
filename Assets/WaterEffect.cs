c3605da500a77502cf0b5f9a2801ef6a62b9f494
Jakob Högerl <jakobhoegerl@gmail.com>
2018-03-09 16:47:20 +0100

    rearranged scene

---
+++ Assets/WaterEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEffect : MonoBehaviour {
+    [Header("Options")]
+    [SerializeField]
+    private float m_offset;
+
+
+    GameObject quad;
+	// Use this for initialization
+	void Start () {
+        quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
